68c34bec8af85bd8b71b1aa0745b3daa0ee3ba45
Dallen Petersen <dallenrpetersen@gmail.com>
2025-07-09 21:37:43 -0600

    Use sqlx query! macros

--- src/database.rs
+++ src/database.rs
@@ -18,40 +18,42 @@ impl Database {
     }
 
     pub async fn get_todos(&self) -> Result<Vec<Todo>, AppError> {
-        let todos = sqlx::query_as::<_, Todo>("SELECT id, description, done FROM todos ORDER BY id")
+        let todos = sqlx::query!("SELECT id, description, done FROM todos ORDER BY id")
             .fetch_all(&self.pool)
-            .await?;
+            .await?
+            .into_iter()
+            .map(|row| Todo {
+                id: row.id,
+                description: row.description,
+                done: row.done,
+            })
+            .collect();
         Ok(todos)
     }
 
     pub async fn add_todo(&self, description: String) -> Result<(), AppError> {
-        sqlx::query("INSERT INTO todos (description) VALUES ($1)")
-            .bind(description)
+        sqlx::query!("INSERT INTO todos (description) VALUES ($1)", description)
             .execute(&self.pool)
             .await?;
         Ok(())
     }
 
     pub async fn toggle_todo(&self, id: i32) -> Result<(), AppError> {
-        sqlx::query("UPDATE todos SET done = NOT done WHERE id = $1")
-            .bind(id)
+        sqlx::query!("UPDATE todos SET done = NOT done WHERE id = $1", id)
             .execute(&self.pool)
             .await?;
         Ok(())
     }
 
     pub async fn delete_todo(&self, id: i32) -> Result<(), AppError> {
-        sqlx::query("DELETE FROM todos WHERE id = $1")
-            .bind(id)
+        sqlx::query!("DELETE FROM todos WHERE id = $1", id)
             .execute(&self.pool)
             .await?;
         Ok(())
     }
 
     pub async fn rename_todo(&self, id: i32, description: String) -> Result<(), AppError> {
-        sqlx::query("UPDATE todos SET description = $1 WHERE id = $2")
-            .bind(description)
-            .bind(id)
+        sqlx::query!("UPDATE todos SET description = $1 WHERE id = $2", description, id)
             .execute(&self.pool)
             .await?;
         Ok(())
--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,11 @@
-use axum::{
-    extract::{Form, Path, State},
-    response::{IntoResponse, Redirect},
-    routing::{get, post},
-    Router,
-};
-use askama::Template;
+
 use dotenvy::dotenv;
 use tokio::net::TcpListener;
 use axum_askama_sample::{
-    database::{Database, Todo},
-    error::AppError,
+    database::Database,
+    routes::todos::create_router,
 };
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
-
-#[derive(Template)]
-#[template(path = "index.html")]
-struct IndexTemplate {
-    todos: Vec<Todo>,
-}
-
-#[derive(serde::Deserialize)]
-struct NewTodo {
-    description: String,
-}
-
-#[derive(serde::Deserialize)]
-struct RenameTodo {
-    description: String,
-}
-
-async fn index(State(database): State<Database>) -> Result<impl IntoResponse, AppError> {
-    let todos = database.get_todos().await?;
-    Ok(IndexTemplate { todos })
-}
-
-async fn add_todo(State(database): State<Database>, Form(new_todo): Form<NewTodo>) -> Result<impl IntoResponse, AppError> {
-    database.add_todo(new_todo.description).await?;
-    Ok(Redirect::to("/"))
-}
-
-async fn toggle_todo(State(database): State<Database>, Path(id): Path<i32>) -> Result<impl IntoResponse, AppError> {
-    database.toggle_todo(id).await?;
-    Ok(Redirect::to("/"))
-}
-
-async fn delete_todo(State(database): State<Database>, Path(id): Path<i32>) -> Result<impl IntoResponse, AppError> {
-    database.delete_todo(id).await?;
-    Ok(Redirect::to("/"))
-}
-
-async fn rename_todo(State(database): State<Database>, Path(id): Path<i32>, Form(todo): Form<RenameTodo>) -> Result<impl IntoResponse, AppError> {
-    database.rename_todo(id, todo.description).await?;
-    Ok(Redirect::to("/"))
-}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -67,12 +20,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let database = Database::new().await?;
 
-    let app = Router::new()
-        .route("/", get(index).post(add_todo))
-        .route("/toggle/:id", post(toggle_todo))
-        .route("/delete/:id", post(delete_todo))
-        .route("/rename/:id", post(rename_todo))
-        .with_state(database);
+    let app = create_router().with_state(database);
 
     let listener = TcpListener::bind("0.0.0.0:3000").await?;
     tracing::debug!("listening on {}", listener.local_addr()?);
