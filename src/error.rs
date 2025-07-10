68486a0a3e74f7a7c7dcfbee431e60d84439e0c8
Dallen Petersen <dallenrpetersen@gmail.com>
2025-07-09 21:41:14 -0600

    Use thiserror for app errors

--- src/error.rs
+++ src/error.rs
@@ -2,26 +2,24 @@ use axum::{
     http::StatusCode,
     response::{IntoResponse, Response},
 };
+use thiserror::Error;
 
+#[derive(Error, Debug)]
 pub enum AppError {
-    Sqlx(sqlx::Error),
+    #[error("Database error: {0}")]
+    Sqlx(#[from] sqlx::Error),
+}
+
+impl AppError {
+    fn status_code(&self) -> StatusCode {
+        match self {
+            AppError::Sqlx(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
 }
 
 impl IntoResponse for AppError {
     fn into_response(self) -> Response {
-        let (status, error_message) = match self {
-            AppError::Sqlx(e) => (
-                StatusCode::INTERNAL_SERVER_ERROR,
-                format!("Database error: {}", e),
-            ),
-        };
-
-        (status, error_message).into_response()
-    }
-}
-
-impl From<sqlx::Error> for AppError {
-    fn from(e: sqlx::Error) -> Self {
-        AppError::Sqlx(e)
+        (self.status_code(), self.to_string()).into_response()
     }
 }
